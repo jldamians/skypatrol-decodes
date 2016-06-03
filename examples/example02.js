c04cc3bce4cef523631ba93ef2c88e5c8045f4c1
jlds <jlds161089@gmail.com>
2016-06-03 15:18:32 -0500

    [update] Ejemplo de envio de comandos

--- examples/example02.js
+++ examples/example02.js
@@ -1,7 +1,8 @@
 'use strict';
 
+var GENERIC_COMMAND_TYPES = require('../index.js').constants.genericCommandTypes;
 var Commands = require('../index.js').commands;
 
-console.log('activateImmobilizer =>', Commands.activateImmobilizer());
-console.log('deactivateImmobilizer =>', Commands.deactivateImmobilizer());
-console.log('eraseLogMemory =>', Commands.eraseLogMemory());
+console.log('activateImmobilizer =>', Commands(GENERIC_COMMAND_TYPES.activateImmobilizer));
+console.log('deactivateImmobilizer =>', Commands(GENERIC_COMMAND_TYPES.deactivateImmobilizer));
+console.log('eraseLogMemory =>', Commands(GENERIC_COMMAND_TYPES.eraseLogMemory));
