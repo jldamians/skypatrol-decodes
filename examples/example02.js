bbc28c89d4c97a403ceb28028b20dd1714a264db
jlds <jlds161089@gmail.com>
2016-06-18 13:53:12 -0500

    [update] Estructura de los módulos para ser exportados con "module.exports"

--- examples/example02.js
+++ examples/example02.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var GENERIC_COMMAND_TYPES = require('../index.js').constants.commandTypes;
-var Commands = require('../index.js').commands;
+var commands = require('../index.js').commands();
 
-console.log('activateImmobilizer =>', Commands(GENERIC_COMMAND_TYPES.activateImmobilizer));
-console.log('deactivateImmobilizer =>', Commands(GENERIC_COMMAND_TYPES.deactivateImmobilizer));
-console.log('eraseLogMemory =>', Commands(GENERIC_COMMAND_TYPES.eraseLogMemory));
+console.log('activateImmobilizer =>', commands(GENERIC_COMMAND_TYPES.activateImmobilizer));
+console.log('deactivateImmobilizer =>', commands(GENERIC_COMMAND_TYPES.deactivateImmobilizer));
+console.log('eraseLogMemory =>', commands(GENERIC_COMMAND_TYPES.eraseLogMemory));
