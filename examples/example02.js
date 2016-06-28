f71e5d4bb24501dc68adc383451b3473efd812d5
jlds <jlds161089@gmail.com>
2016-06-27 22:19:59 -0500

    [update] Definir la estructura, en función al patrón módulo

--- examples/example02.js
+++ examples/example02.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var GENERIC_COMMAND_TYPES = require('../index.js').constants.commandTypes;
-var commands = require('../index.js').commands();
+var commands = require('../index.js').commands;
 
 console.log('activateImmobilizer =>', commands(GENERIC_COMMAND_TYPES.activateImmobilizer));
 console.log('deactivateImmobilizer =>', commands(GENERIC_COMMAND_TYPES.deactivateImmobilizer));
