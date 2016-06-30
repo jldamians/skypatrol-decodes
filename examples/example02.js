ab52b6a99eb41b44406e214006f7318a040de9d7
Jose Luis <jlds161089@gmail.com>
2016-06-30 09:16:47 -0500

    [update] Refactorizando con prototipos

--- examples/example02.js
+++ examples/example02.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var GENERIC_COMMAND_TYPES = require('../index.js').constants.commandTypes;
-var Commands = require('../index.js').commands;
+var Command = require('../index.js').Command;
 
-console.log('activateImmobilizer =>', Commands(GENERIC_COMMAND_TYPES.activateImmobilizer));
-console.log('deactivateImmobilizer =>', Commands(GENERIC_COMMAND_TYPES.deactivateImmobilizer));
-console.log('eraseLogMemory =>', Commands(GENERIC_COMMAND_TYPES.eraseLogMemory));
+console.log('activateImmobilizer =>', new Command(GENERIC_COMMAND_TYPES.activateImmobilizer));
+console.log('deactivateImmobilizer =>', new Command(GENERIC_COMMAND_TYPES.deactivateImmobilizer));
+console.log('eraseLogMemory =>', new Command(GENERIC_COMMAND_TYPES.eraseLogMemory));
